5292b291a93c3fc020827b2b96431b8e28e55083
chrisevans444 <chrisevans444@065df920-e8c9-11dd-9b7c-bbba4c9295f9>
2010-03-18 08:56:57 +0000

    Updated file header comments. Removed some redundant code from main.

--- trunk/OpenSURF2/ipoint.cpp
+++ trunk/OpenSURF2/ipoint.cpp
@@ -1,3 +1,14 @@
+/*********************************************************** 
+*  --- OpenSURF2 ---                                       *
+*  This library is distributed under the GNU GPL. Please   *
+*  use the contact form at http://www.chrisevansdev.com    *
+*  for more information.                                   *
+*                                                          *
+*  C. Evans, Research Into Robust Visual Features,         *
+*  MSc University of Bristol, 2008.                        *
+*                                                          *
+************************************************************/
+
 #include <cv.h>
 #include <vector>
 
--- trunk/OpenSURF2/responselayer.h
+++ trunk/OpenSURF2/responselayer.h
@@ -1,3 +1,15 @@
+/*********************************************************** 
+*  --- OpenSURF2 ---                                       *
+*  This library is distributed under the GNU GPL. Please   *
+*  use the contact form at http://www.chrisevansdev.com    *
+*  for more information.                                   *
+*                                                          *
+*  C. Evans, Research Into Robust Visual Features,         *
+*  MSc University of Bristol, 2008.                        *
+*                                                          *
+************************************************************/
+
+
 //#define RL_DEBUG  // un-comment to test response layer
 
 class ResponseLayer
